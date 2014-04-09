5add2f9c69425e22e39461457dbd8cb39559a0ce
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-04-08 17:52:06 -0700

    add stop and run commands

---
+++ mcmtestOpenTK/mcmtestOpenTK/Client/CommandHandlers/QueueCmds/StopCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using mcmtestOpenTK.Shared;
+using mcmtestOpenTK.Client.UIHandlers;
+
+namespace mcmtestOpenTK.Client.CommandHandlers.QueueCmds
+{
+    class StopCommand: AbstractCommand
+    {
+        public StopCommand()
+        {
+            Name = "stop";
+            Arguments = "";
+            Description = "Stops the current command queue.";
+        }
+
+        public override void Execute(CommandInfo info)
+        {
+            UIConsole.WriteLine(TextStyle.Color_Outgood + "Stopping current queue.");
+            info.Queue.Stop();
+        }
+    }
+}
